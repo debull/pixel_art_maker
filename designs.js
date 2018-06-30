a97503c5bbc878a8d5c890dc71c87b7c195f3ea4
Debull-Effiong <f.young.de@gmail.com>
2018-07-01 00:20:48 +0100

    Limit max rows and columns to 100. Added double click to remove background color

--- designs.js
+++ designs.js
@@ -24,23 +24,37 @@ function makeGrid() {
     let getRow = parseInt( tableHeight.val() );
     let getCol = parseInt( tableWidth.val() );
 
-    // Loop through user's input values and generate rows and cols as entered. 
-    for(let y = 0; y < getRow; y++){
-        let createRow = $('<tr></tr>');
-     
-        for(let x = 0; x < getCol; x++){
-            let createCol = $('<td></td');
-            createRow.append(createCol);
-        }   
+    // Check and limit user's input values to between 1 - 100
+    if( (getRow < 1 || getRow > 100) || (getCol < 1 || getCol > 100) ) {
+        alert("Enter a number between 1 - 100.");
+    } else {
 
-        pixelCanvas.append(createRow);
+        // Loop through user's input values and generate rows and cols as entered. 
+        for(let y = 0; y < getRow; y++){
+     
+                let createRow = $('<tr></tr>');
+             
+                for(let x = 0; x < getCol; x++){
+                    let createCol = $('<td></td');
+                    createRow.append(createCol);
+                }   
+        
+                pixelCanvas.append(createRow);
+        }
     }
+
 
     // Add an on click event listener to #pixelCanvas
     // Use event delegation to target each cell of the table
     // to change background-color to the picked color on click.
-    pixelCanvas.on('click', 'td', function(evt) {
-        $(evt.target).css('background-color', selectedColor.val());
+    pixelCanvas.on('click', 'td', function(event) {
+        let gbColor = $(event.target).css('background-color', selectedColor.val());
+
+        // Add Double click to remove background color
+        $(this).dblclick(() => {
+            $(this).css('background-color', 'transparent');
+        });
+        
     });
 
 }
